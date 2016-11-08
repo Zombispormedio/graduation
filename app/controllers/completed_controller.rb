7e5cd0fcdc1647dd25eb11284e5c987d0e3a8a5b
Xavier Serrano <zombispormedio007@gmail.com>
2016-11-08 19:05:10 +0100

    finish and maker

--- app/controllers/completed_controller.rb
+++ app/controllers/completed_controller.rb
@@ -1,9 +1,7 @@
 class CompletedController < ApplicationController
   def index
-=begin
     if flash[:redirect] != "by_student_created"
       redirect_to root_path
     end
-=end
   end
 end
--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,16 +1,17 @@
-require 'google/apis/sheets_v4'
-require 'googleauth'
-require 'googleauth/stores/redis_token_store'
+require 'unirest'
 
 class StudentController < ApplicationController
 
   def index
     @new_student=Student.new
+    @students_count=Student.count
   end
 
   def create
+
     @new_student=Student.new(params[:new_student].permit(:name, :surname, :email))
     if @new_student.save
+      append_to_sheet(@new_student.name+" "+@new_student.surname, @new_student.email)
       flash[:redirect] ="by_student_created"
       redirect_to completed_path
     else
@@ -21,8 +22,11 @@ class StudentController < ApplicationController
 
   private
   def append_to_sheet(name, email)
-
-
+    p ENV["GRADUATION_MAKER"]
+    response = Unirest.post ENV["GRADUATION_MAKER"],
+                            headers:{ "Accept" => "application/json" },
+                            parameters:{ :value1=>name, :value2=>email }
+    p response
 
   end
 
